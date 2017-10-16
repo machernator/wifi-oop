5d2746a55bcdae05acd076c023067b2bf2f7ec2b
WIFI-IT\machert <machernator@gmail.com>
2017-10-16 10:04:24 +0200

    Zugriffsmodifikatoren hinzugefügt

--- inc/Person.class.php
+++ inc/Person.class.php
@@ -2,15 +2,21 @@
 // Mit dem Schlüsselwort class werden Klassen erstellt
 class Person
 {
-    // Eigenschaften/Attributes der Klasse (Variablen)
-    $kdnr = '';
-    $anrede = '';
-    $titel = '';
-    $vorname = '';
-    $nachname = '';
+    /* 
+        Eigenschaften/Attributes der Klasse (Variablen)
+        Zugriffsmodifikatoren regeln, welche Eigenschaften und 
+        Methoden von außen zugänglich sein sollen.
+        private, protected: es kann nicht von außen zugegriffen werden
+        public: von außen ist der Zugriff möglich 
+    */
+    private $kdnr = '';
+    private $anrede = '';
+    private $titel = '';
+    private $vorname = '';
+    private $nachname = '';
 
     // Methoden der Klasse (Funktionen)
-    function greeting() {
+    public function greeting() {
         echo "Greeting";
     }
 }
